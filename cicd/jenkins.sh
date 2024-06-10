22f114ca9e042db7708cdc913d82fd67590ab862
shravan <shravankumar6262@gmail.com>
2024-06-11 00:57:41 +0530

    pipeline

--- cicd/jenkins.sh
+++ cicd/jenkins.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-yum install fontconfig java-17-openjdk -y
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install terraform
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install fontconfig java-17-openjdk jenkins -y
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
